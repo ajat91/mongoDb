74080c8a02f6839f6beda7ac7ade69cb0e805fe5
ajat91.sudrajat <ajat91.sudrajat@gmail.com>
2023-01-02 17:08:55 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import  express  from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import userRouter from "./routes/userRoute.js";
+
+const app = express();
+mongoose.connect();
+
+const db=mongoose.connection;
+db.on('error',(e)=>console.log(e))
+db.once('open',()=>console.log('Database Terhubung'))
+
+
+app.use(cors());
+app.use(express.json());
+app.use(userRouter);
+
+app.listen(5000,()=>console.log("Server up and running"))
